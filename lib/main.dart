7777cec5678edc7c4fd6a86458cc0865657bf380
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-05-24 21:36:23 +0900

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -26,22 +26,72 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('hellow'),
+              accountEmail: Icon(Icons.send),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/img.jpg'),
+              ),
+              onDetailsPressed: () {
+                print('detail');
+              },
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.red.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 5),
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.send,
+                color: Colors.red,
+              ),
+              title: Text('리스트'),
+              onTap: () {
+                print('리스트');
+              },
+              trailing: IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  print('add');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('hellow'),
       ),
       body: Column(
         children: [
           ElevatedButton(
-              onPressed: () {},
-              child: Text('elevated',style: TextStyle(color: Colors.black),),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow,
-                  textStyle:TextStyle(
-                    backgroundColor: Colors.blue,
-                    fontSize: 20.0,
-
-                  ),
+            onPressed: () {},
+            child: Text(
+              'elevated',
+              style: TextStyle(color: Colors.black),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.yellow,
+              textStyle: TextStyle(
+                backgroundColor: Colors.blue,
+                fontSize: 20.0,
               ),
+            ),
           ),
           TextButton(onPressed: () {}, child: Text('TextButton')),
           OutlinedButton(onPressed: () {}, child: Text('OutlinedButton'))
