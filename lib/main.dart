b4fcfa9699bfa83c0e4362ee881b8aec753fb1fa
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-05-26 21:22:13 +0900

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+void main()=>runApp(MyApp());
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,147 +17,67 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: true,
-      home: MyHome(),
+      home: stf(),
     );
   }
 }
 
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
+
+class stf extends StatefulWidget {
+  const stf({Key? key}) : super(key: key);
+
+  @override
+  State<stf> createState() => _stfState();
+}
+
+class _stfState extends State<stf> {
 
   @override
   Widget build(BuildContext context) {
+    int a=0;
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('hellow'),
-              accountEmail: Icon(Icons.send),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/img.jpg'),
-              ),
-              onDetailsPressed: () {
-                print('detail');
-              },
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.red.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 5),
-                  )
-                ],
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.send,
-                color: Colors.red,
-              ),
-              title: Text('리스트'),
-              onTap: () {
-                print('리스트');
-              },
-              trailing: IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  print('add');
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
       appBar: AppBar(
-        title: Text('hellow'),
+        title: Text('stateful'),
       ),
       body: Column(
         children: [
           ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('hi'),
-                  duration: Duration(seconds: 5),
-                ),
-              );
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => second(),
-                ),
-              );
-            },
-            child: Text(
-              'elevated',
-              style: TextStyle(color: Colors.black),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.yellow,
-              textStyle: TextStyle(
-                backgroundColor: Colors.blue,
-                fontSize: 20.0,
-              ),
-            ),
+              onPressed: (){
+                setState(() {
+                  a=a+1;
+                });
+                print(a);
+              },
+              child: Text('숫자증가'),
           ),
-          TextButton(onPressed: () {}, child: Text('TextButton')),
-          OutlinedButton(onPressed: () {}, child: Text('OutlinedButton'))
+          Text('$a'),
         ],
       ),
     );
   }
 }
 
-class second extends StatelessWidget {
-  const second({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ScaffoldMessenger(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('second'),
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => third(),
-                  ),
-                );
-              },
-              child: Text('jo'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class third extends StatelessWidget {
-  const third({Key? key}) : super(key: key);
-
+class prastl extends StatelessWidget {
+  prastl({Key? key}) : super(key: key);
+  int a=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('third'),
+        title: Text('stateful'),
       ),
       body: Column(
         children: [
-          Text('hi'),
+          ElevatedButton(
+            onPressed: (){
+
+                a=a+1;
+
+              print(a);
+            },
+            child: Text('숫자증가'),
+          ),
+          Text('$a'),
         ],
       ),
     );
